5031214530a64259d4ec53b45a919c2894fa04ef
Douwenga <mariettedouwenga@gmail.com>
2025-03-25 14:52:46 +0100

    Add InviteNewUsersList.py and NewUsers_2025 excel file

---
+++ InviteNewUsersList.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import streamlit as st
+
+
+file_path = "NewUsers_2025.xlsx"
+column_name = st.text_input("Enter the training name")
+
+def find_last_empty_row(file_path, column_name):
+    # Read the Excel file
+    df = pd.read_excel(file_path)
+    
+    # Check if the column exists
+    if column_name not in df.columns:
+        raise ValueError(f"Column '{column_name}' does not exist in the dataframe.")
+    
+    # Get the column data
+    column_data = df[column_name]
+    
+    # Find the first empty row in the column
+    first_empty_row = column_data[column_data.isna()].index[0]
+    
+    return first_empty_row
+
+#Validation of first empty row in a specific Column
+try:
+    last_empty_row = find_last_empty_row(file_path, column_name)
+    print(f"The first empty row in column '{column_name}' is: {last_empty_row}")
+except ValueError as e:
+    print(e)
+except IndexError:
+    print(f"No empty rows found in column '{column_name}'")
