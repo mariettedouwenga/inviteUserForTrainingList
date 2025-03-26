88c2622336886444f905b8b3dcf0f1fb655f035d
mariettedouwenga <mariettedouwenga@gmail.com>
2025-03-26 09:29:18 +0100

    Added training date input box

--- InviteNewUsersList.py
+++ InviteNewUsersList.py
@@ -12,14 +12,16 @@ with header:
     st.divider()
 
 file_path = "NewUsers_2025.xlsx"
-#column_name = st.text_input("Enter the training name")
+training_names = pd.read_excel("trainingName.xlsx")
 
-column_name = pd.read_excel('trainingName.xlsx')
-
-role = st.selectbox('Select a role', column_name, index = None,
+#Select the training name
+column_name = st.selectbox('Select the training name', training_names, index = None,
     placeholder="Select training name",)
 
-def find_last_empty_row(file_path, column_name):
+#Input to enter the date on which the training will take place
+training_date = st.date_input("Enter the training date")
+
+def find_first_empty_row(file_path, column_name):
     # Read the Excel file
     df = pd.read_excel(file_path)
     
@@ -32,14 +34,20 @@ def find_last_empty_row(file_path, column_name):
     
     # Find the first empty row in the column
     first_empty_row = column_data[column_data.isna()].index[0]
+    #st.write(str(training_date.value))
     
     return first_empty_row
 
 #Validation of first empty row in a specific Column
 try:
-    last_empty_row = find_last_empty_row(file_path, column_name)
-    print(f"The first empty row in column '{column_name}' is: {last_empty_row}")
+    first_empty_row = find_first_empty_row(file_path, column_name)
+    print(f"The first empty row in column '{column_name}' is: {first_empty_row}")
 except ValueError as e:
     print(e)
 except IndexError:
     print(f"No empty rows found in column '{column_name}'")
+
+
+
+
+#Add the training_date to the first empty row of the training name column
